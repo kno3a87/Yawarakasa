78a62c5db948e5393ae332e9483ef2a19eb85937
Kuno <jazzy.kno3.wonder@gmail.com>
2020-03-12 18:32:41 +0900

    データ追加

--- yawarakasa-create.sql
+++ yawarakasa-create.sql
@@ -1,3 +1,6 @@
 # UserID varchar(255), year int(4), month int(2), day int(2), yawarakasa int(255)
 
 insert into yawarakasa values ('Kuno', 2020, 2, 24, 30);
+insert into yawarakasa values ('Kuno', 2020, 2, 25, 31);
+insert into yawarakasa values ('Kuno', 2020, 2, 26, 28);
+insert into yawarakasa values ('Kuno', 2020, 2, 27, 33);
