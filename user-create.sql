b0123c95e5131fca5f352bffc54d1fe9c61b5cb7
Kuno <jazzy.kno3.wonder@gmail.com>
2020-03-12 18:19:46 +0900

    テーブルに情報追加してみた

---
+++ user-create.sql
@@ -0,0 +1,3 @@
+# UserID varchar(255), Sex char(1)
+
+insert into user values ('Kuno', 'f');
---
+++ yawarakasa-create.sql
@@ -0,0 +1,3 @@
+# UserID varchar(255), year int(4), month int(2), day int(2), yawarakasa int(255)
+
+insert into yawarakasa values ('Kuno', 2020, 2, 24, 30);
