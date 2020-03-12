6fd16bb5dd5fbc310a800c95a4a8c45020444dd7
Kuno <jazzy.kno3.wonder@gmail.com>
2020-03-12 18:37:31 +0900

    ユーザ追加

--- user-create.sql
+++ user-create.sql
@@ -1,3 +1,6 @@
 # UserID varchar(255), Sex char(1)
 
 insert into user values ('Kuno', 'f');
+insert into user values ('papa', 'm');
+insert into user values ('mama', 'f');
+insert into user values ('Hochiko', 'f');
