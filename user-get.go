b9e8f61766bd0826172c49adeeb3aa0e1aef754b
Kuno <jazzy.kno3.wonder@gmail.com>
2020-03-13 15:44:27 +0900

    user_id指定無しで情報みるだけのAPI

--- user-get.go
+++ user-get.go
@@ -4,31 +4,24 @@ import (
   "database/sql"
   "fmt"
   _ "github.com/go-sql-driver/mysql"
+  "log"
+  "net/http"
 )
 
 type User struct {
-  user_id   string
+  user_id string
   sex string
 }
 
-func main() {
+// ユーザ情報を取得・表示
+func drawUser(w http.ResponseWriter, r *http.Request){
   // rootで作ったのでパスワードなし
   db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/YawarakasaApp")
   if err != nil {
     panic(err.Error())
   }
   defer db.Close()
-
-  // TerminalからID打ち込み
-  var id string
-  print("user_id : ")
-  fmt.Scan(&id)
-
-  // queryにID入りの文代入
-  var query string
-  query = "select * from user where user_id = '" + id + "'"
-  // println(query) // debug用
-  rows, err := db.Query(query)
+  rows, err := db.Query("select * from user where user_id = 'Kuno'")
 
   if err != nil {
     panic(err.Error())
@@ -41,11 +34,30 @@ func main() {
     if err != nil {
       panic(err.Error())
     }
-    fmt.Println(user.user_id, user.sex)
+    fmt.Fprintf(w, "user_id : " + user.user_id)
+    fmt.Fprintf(w, ", sex : " + user.sex)
   }
 
   err = rows.Err()
   if err != nil {
     panic(err.Error())
   }
+
+  // Terminalにログ表示
+  fmt.Println("Endpoint Hit: drawUser")
+}
+
+func hello(w http.ResponseWriter, r *http.Request){
+  fmt.Fprintf(w, "ユーザ情報をみるなら/user/get?id=???と入力してね")
+  fmt.Println("Endpoint Hit: Hello")
+}
+
+func handleRequests() {
+  http.HandleFunc("/", hello)
+  http.HandleFunc("/user/get", drawUser)
+  log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func main() {
+  handleRequests()
 }
