dbfb90cca7487e8248facdd7f7e90e179ace2c1c
Kuno <jazzy.kno3.wonder@gmail.com>
2020-03-23 15:33:10 +0900

    jsonに書き出せない…

--- pkg/api/handler/user/handler.go
+++ pkg/api/handler/user/handler.go
@@ -4,7 +4,7 @@
 package HandlerPkg
 
 import (
-  //"encoding/json"
+  "encoding/json"
   "database/sql"
   "fmt"
   _ "github.com/go-sql-driver/mysql"
@@ -30,23 +30,41 @@ func GetUser(w http.ResponseWriter, r *http.Request) {
   }
   defer db.Close()
 
-/*
-  // queryにクエリパラメータ代入
-  var query string
-  query = "select * from user where user_id = '" + r.URL.RawQuery + "'"
-  rows, err := db.Query(query)
-
-  if err != nil {
-    panic(err.Error())
-    return
+  url := ""
+  request, err := http.NewRequest("GET", url, nil)
+  if err != nil{
+    //log.Fatal(err)
   }
-  defer rows.Close()
 
+  //クエリパラメータ
+  params := request.URL.Query()
+  params.Add("userId","Kuno") // 直接指定してみる
+  request.URL.RawQuery = params.Encode()
+
+  // ブラウザにjson表示
+  //fmt.Fprintf(writer, )
+
+  // Terminalにログ表示
+  fmt.Println(params) // 'map[userId:[Kuno]]'
+  fmt.Println(params["userId"]) // '[Kuno]'
+  fmt.Println(request.URL.String())  // '?userId=Kuno'
+
+
+  // MySQLからuserId=Kunoの情報取ってくる
+  var query_sentence string
+  query_sentence = "select * from user where user_id = '" + params["userId"] + "'"
+  rows, err := db.Query(query_sentence)
   for rows.Next() {
     var user User
     err := rows.Scan(&user.user_id, &user.sex)
 
-    json.NewEncoder(w).Encode(user)
+    //json.NewEncoder(w).Encode(user)
+    // jsonエンコード
+    outputJson, err := json.Marshal(&user)
+    if err != nil {
+      panic(err)
+    }
+    fmt.Fprint(w, string(outputJson))
 
     if err != nil {
       panic(err.Error())
@@ -55,27 +73,6 @@ func GetUser(w http.ResponseWriter, r *http.Request) {
     //fmt.Fprintf(w, "user_id : " + user.user_id)
     //fmt.Fprintf(w, ", sex : " + user.sex)
   }
-
-  err = rows.Err()
-  if err != nil {
-    panic(err.Error())
-    return
-  }*/
-
-  //url := "https://jsonplaceholder.typicode.com/todos"
-  url := "" // ここなに？？？？？
-
-	request, err := http.NewRequest("GET", url, nil)
-	if err != nil{
-		//log.Fatal(err)
-	}
-
-	//クエリパラメータ
-	params := request.URL.Query()
-    params.Add("userId","1")
-    request.URL.RawQuery = params.Encode()
-
-	fmt.Println(request.URL.String()) //https://jsonplaceholder.typicode.com/todos?userId=1
 
   // Terminalにログ表示
   fmt.Println("Endpoint Hit: drawUser")
